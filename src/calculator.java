3e2789e2d1f6acdbd979fd01089257c861c2d038
Min-92 <0201.mj.lee@gmail.com>
2019-11-26 16:02:35 +0900

    사칙연산 조건문

--- src/calculator.java
+++ src/calculator.java
@@ -9,11 +9,22 @@ public class calculator {
         System.out.println("두 번쨰 값 입력");
         int b = sc.nextInt();
 
+        System.out.println("사칙연산?");
+        String symbol = sc.next();
 
-        System.out.println("덧셈 : " + (a+b));
-        System.out.println("뺼셈 : " + (a-b));
-        System.out.println("곱셈 : " + (a*b));
-        System.out.println("나눗셈 : " + (a/b));
+        if ("+".equals(symbol)) {
+            System.out.println("덧셈 : " + (a + b));
+        } else if ("-".equals(symbol)) {
+            System.out.println("뺼셈 : " + (a - b));
+        } else if ("*".equals(symbol)) {
+            System.out.println("곱셈 : " + (a * b));
+
+        } else if ("/".equals(symbol)) {
+            System.out.println("나눗셈 : " + (a / b));
+        } else {
+            System.out.println("+-*/ 중 하나를 입력해주세요");
+        }
+
 
     }
 }
