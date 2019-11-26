2f74c849beb5a099f7b3ebede2ba759d198079f1
Min-92 <0201.mj.lee@gmail.com>
2019-11-26 14:25:50 +0900

    구구단 조건문

--- src/gugudan.java
+++ src/gugudan.java
@@ -3,8 +3,16 @@ import java.util.Scanner;
 public class gugudan {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-
         int i = sc.nextInt();
+
+        if (i < 2) {
+            System.out.printf("2 이상 입력해야 합니다.");
+            return;
+        } else if (i > 9) {
+            System.out.println("9 이하 입력해야 합니다.");
+            return;
+        }
+
         for (int j = 1; j < 10; j++) {
             System.out.println(i + "*" + j + " 은 " + i * j);
         }
