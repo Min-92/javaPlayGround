501adef53b9b5e965531d606cdf320c606a67198
Min-92 <0201.mj.lee@gmail.com>
2019-11-26 14:23:48 +0900

    구구단 입력받기

--- src/gugudan.java
+++ src/gugudan.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class gugudan {
     public static void main(String args[]) {
-        for (int i = 2; i < 4; i++) {
-            for (int j = 1; j < 10; j++) {
-                System.out.println(i+"*"+j+" 은 "+i*j);
-            }
+        Scanner sc = new Scanner(System.in);
+
+        int i = sc.nextInt();
+        for (int j = 1; j < 10; j++) {
+            System.out.println(i + "*" + j + " 은 " + i * j);
         }
 
 
